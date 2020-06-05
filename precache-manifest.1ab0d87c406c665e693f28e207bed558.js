f08d52ed4cf20921f7734bb9f4d4cffda25c4d6f
virinchi123 <raovirinchi123@gmail.com>
2020-06-05 10:32:28 +0530

    Updates

---
+++ precache-manifest.1ab0d87c406c665e693f28e207bed558.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b690c2caa9d4b2f73757dec0c78b06",
+    "url": "/Smarter-Parker/index.html"
+  },
+  {
+    "revision": "b0cf2835b2483622b39d",
+    "url": "/Smarter-Parker/static/css/main.b84762e7.chunk.css"
+  },
+  {
+    "revision": "4f91bed78b2b940b5970",
+    "url": "/Smarter-Parker/static/js/2.ab6b8e6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Smarter-Parker/static/js/2.ab6b8e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0cf2835b2483622b39d",
+    "url": "/Smarter-Parker/static/js/main.e1a3d841.chunk.js"
+  },
+  {
+    "revision": "d4b6c87a4be33ea4580f",
+    "url": "/Smarter-Parker/static/js/runtime-main.dc1b379a.js"
+  },
+  {
+    "revision": "eb8c9c1625fcb08ca7bfc046f015ea87",
+    "url": "/Smarter-Parker/static/media/bg-intro-desktop.eb8c9c16.png"
+  },
+  {
+    "revision": "3e78c71a68ca0575e5ffb30e2e8ccc69",
+    "url": "/Smarter-Parker/static/media/bg-intro-mobile.3e78c71a.png"
+  },
+  {
+    "revision": "8e4a5574d019096b62a948e62e920308",
+    "url": "/Smarter-Parker/static/media/icon-calculator.8e4a5574.svg"
+  },
+  {
+    "revision": "ae2dc13dd8be5d72dc6ea67236f8729c",
+    "url": "/Smarter-Parker/static/media/icon-karma.ae2dc13d.svg"
+  },
+  {
+    "revision": "8f7824b371e618577b20ba3775d9cfb4",
+    "url": "/Smarter-Parker/static/media/icon-supervisor.8f7824b3.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Smarter-Parker/precache-manifest.1ab0d87c406c665e693f28e207bed558.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Smarter-Parker/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
